0e568815491281ff3f9c43aa7354ae919a7c3d87
Michael Pogoda <do0dlez1991@gmail.com>
2012-12-02 21:17:33 +0200

    Finished lab3

--- main_widget.cxx
+++ main_widget.cxx
@@ -4,6 +4,7 @@
 
 #include "lab1/lab1_widget.hxx"
 #include "lab2/lab2_widget.hxx"
+#include "lab3/lab3_widget.hxx"
 
 Main_Widget::Main_Widget(QWidget *parent) :
   QWidget(parent),
@@ -14,6 +15,7 @@ Main_Widget::Main_Widget(QWidget *parent) :
 {
   labs.push_back(new Lab1_Widget(this));
   labs.push_back(new Lab2_Widget(this));
+  labs.push_back(new Lab3_Widget(this));
 
   buttons->setExclusive(true);
 
